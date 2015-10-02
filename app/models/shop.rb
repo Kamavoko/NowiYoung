42ee71fa8f81db83341d1c770c3b88fde4936946
Kamavoko <tamura.1986@gmail.com>
2015-10-02 15:15:32 +0900

    ２回めのコミット

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,7 +4,7 @@ class Shop < ActiveRecord::Base
   belongs_to :category
 #  has_many photo
   validates :name,
-  presence: {message: "コラコラコラコラ～ッ！(`o´)"}
+  presence: {message: "ショップ名は空欄に出来ませんコラコラコラコラ～ッ！(`o´)"}
 
   mount_uploader :image, ImageUploader
 
